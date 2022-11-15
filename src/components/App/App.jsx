74a91ac326aedb276fc201ef9ed3cae21fb95bd4
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-14 18:30:14 -0600

    created routes for all pages minus stretch pages, also created home component and imported with nav test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import Home from '../Home/Home';
 
 import './App.css';
 
@@ -36,84 +37,121 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
+            {/* --SHOW THE REGISTRATION PAGE */}
+            <Route
+            exact
+            path="/registration"
+            >
+            {user.id ?
+                // If the user is already logged in, 
+                // redirect them to the /user page
+                <Redirect to="/home" />
+                :
+                // Otherwise, show the registration page
+                <RegisterPage />
+            }
+            </Route>
+
+            {/* --SHOW LOGIN PAGE-- */}
+            <Route
+            exact
+            path="/login"
+            >
+            {user.id ?
+                // If the user is already logged in, 
+                // redirect to the /user page
+                <Redirect to="/home" />
+                :
+                // Otherwise, show the login page
+                <LoginPage />
+            }
+            </Route>
+
+            {/* --HOME/POSTS HOME */}
+            <ProtectedRoute
+            exact
+            path="/home"
+            >
+                <Home/>
+            </ProtectedRoute>
+
+            {/* --CREATE/POST */}
+            <ProtectedRoute
+            exact
+            path="/newpost"
+            >
+
+            </ProtectedRoute>
+
+
+            {/* --POST/DETAILS PAGE--             
+                --contains comment and comment edit components--
+            */}
+          
+            <ProtectedRoute
+            exact
+            path="/post/:id"
+            >
+            </ProtectedRoute>
+            
+            {/* edit/ ensure userid verification */}
+            <ProtectedRoute
+            exact
+            path="/post/edit/:id"
+            >
+            </ProtectedRoute>
+
+            {/* --STRETCH: PROFILE PAGE */}
+
+
+
+            {/* --STRETCH: PROFILE EDIT */}
+
+
+
+            {/* Visiting localhost:3000/about will show the about page. */}
+            <Route
             // shows AboutPage at all times (logged in or not)
             exact
             path="/about"
-          >
+            >
             <AboutPage />
-          </Route>
+            </Route>
+            
 
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
 
-          <ProtectedRoute
+
+            {/* <ProtectedRoute
             // logged in shows InfoPage else shows LoginPage
             exact
             path="/info"
-          >
+            ></ProtectedRoute> */}
+
+            {/* If none of the other routes matched, we will show a 404. */}
+
+                    {/* For protected routes, the view could show one of several things on the same route.
+            Visiting localhost:3000/user will show the UserPage if the user is logged in.
+            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
+            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+            {/* <ProtectedRoute
+            // logged in shows UserPage else shows LoginPage
+            exact
+            path="/user"
+            >
+            <UserPage />
+            </ProtectedRoute> */}
+
+            {/* <ProtectedRoute>
             <InfoPage />
-          </ProtectedRoute>
+            </ProtectedRoute> */}
 
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            }
-          </Route>
-
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
+            <Route>
             <h1>404</h1>
-          </Route>
+            </Route>
+
         </Switch>
         <Footer />
       </div>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Nav from "../Nav/Nav";
+
+
+function Home(){
+
+
+    return(
+        <>
+            <h1> IN HOME </h1>
+        </>
+    );
+}
+
+
+export default Home;
