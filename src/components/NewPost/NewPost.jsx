987bc7c2c2666b8f3bc101a64892666ff54d96f6
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-22 10:55:24 -0600

    moved original format

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -106,128 +106,6 @@ function NewPost(){
         <>
             <h3>Create New Post</h3>
 
-            {/* <article> */}
-            
-            {/* add post form info */}
-            {/* <form onSubmit={submitHunt}> */}
-                {/* Image container and input */}
-                {/* {imgUrl.length>0 ? <img  className="preview" src={imgUrl}></img>: <figure>
-                    <Input type='file' name="post_img" 
-                    onChange = {changeHandler}
-                    //original==onChange={(evt)=>setImage(evt.target.files[0])}
-                    />
-                </figure>} */}
-                {/* header with  Title Input, profile name, date*/}
-                {/* <header>
-                    <Input 
-                    type='Text' 
-                    placeholder='Post Title' 
-                    value={postTitle} 
-                    onChange={(evt)=>setPostTitle(evt.target.value)} 
-                    required/>
-                    <p> {user.username} </p>
-                    <p> {moment().format("MMM Do YYYY")}</p>
-                </header> */}
-
-                
-                {/* section with  species, success, date of the hunt*/}
-                {/* <section>
-
-                    <InputLabel>Date of Hunt</InputLabel>
-                        <Input 
-                            required 
-                            value={date} 
-                            onChange={(evt)=>setDate(evt.target.value)} 
-                            type='date'>                                
-                        </Input>
-
-                    <InputLabel>Species</InputLabel>
-                    <Input 
-                        type='text' 
-                        value={species} 
-                        onChange={(evt)=>setSpecies(evt.target.value)} 
-                        placeholder='Species' 
-                        required>                    
-                    </Input>
-                    
-                    <FormControl fullWidth>
-                        <InputLabel>Succesful Hunt?</InputLabel>
-                            <Select
-                                required
-                                labelId="successful-input-label"
-                                value={successful}
-                                label="successful-hunt"
-                                onChange={(evt)=>setSuccessful(evt.target.value)}
-                            >
-                                <MenuItem value={true}>Yes</MenuItem>
-                                <MenuItem value={false}>No</MenuItem>
-                            </Select>
-                    </FormControl>
-
-                </section> */}
-
-                {/* section with location, weapon, land type  */}
-                {/* <section>
-                    <FormControl fullWidth>
-                        <InputLabel required id="huntarea-input-label">Hunt Area</InputLabel>
-                            <Select
-                                labelId="huntarea-input-label"
-                                value={huntAreaId ? huntAreaId : ''}
-                                label="hunt-area"
-                                onChange={(evt)=>setHuntAreaId(evt.target.value)}
-                            >
-                                {huntAreaList.map((area)=>{
-                                    return <MenuItem key={area.id} value={area.id}>{area.hunt_area}</MenuItem>
-                                })}
-
-                            </Select>
-                    </FormControl>
-
-                    <InputLabel>Weapon Used</InputLabel>
-                    <Input type='text' value={weaponType} onChange={(evt)=>setWeaponType(evt.target.value)} placeholder='weapon-used'></Input>
-
-                    <FormControl fullWidth>
-                        <InputLabel id="land-type-input-label">Land Type</InputLabel>
-                            <Select
-                                labelId="land-type-input-label"
-                                value={landType}
-                                label="land-type"
-                                onChange={(evt)=>setLandType(evt.target.value)}
-                            >
-                                <MenuItem value={'public'}>Public</MenuItem>
-                                <MenuItem value={'private'}>Private</MenuItem>
-                            </Select>
-                    </FormControl>
-                </section> */}
-
-                {/* section with text input and store */}
-                {/* <section>
-                <TextareaAutosize
-                    required
-                    placeholder="Tell the story..."
-                    style={{ width: 500, height:200 }}
-                    value={story} onChange={(evt)=>setStory(evt.target.value)}
-                />
-
-                </section> */}
-                {/* eventually have a sweet alert for submit */}
-                {/* <button type="submit">Post</button>
-                <button type='submit' className='submitBtn'>Submit</button>
-                <button className='cancelBtn' onClick={()=>history.push(`/home`)}>Cancel</button>
-            </form> */}
-
-            {/* </article>
-            image cropper activated by change handler
-            {selectedFile ? 
-                <ImageCropper 
-                    id={selectedFile.id} 
-                    imageUrl={selectedFile.imageUrl}
-                    setFinalFile={setFinalFile}
-                    setSelectedFile={setSelectedFile}
-                    setImgUrl={setImgUrl}
-                    // setCroppedImageFor={setCroppedImageFor}
-                /> : null}
-        */}
         
         <form onSubmit={submitHunt}>
             <article className='postDetailsContainer'>
@@ -388,3 +266,128 @@ function NewPost(){
     
 
 export default NewPost;
+
+
+//Original format if there are issues
+{/* <article> */}
+            
+            {/* add post form info */}
+            {/* <form onSubmit={submitHunt}> */}
+                {/* Image container and input */}
+                {/* {imgUrl.length>0 ? <img  className="preview" src={imgUrl}></img>: <figure>
+                    <Input type='file' name="post_img" 
+                    onChange = {changeHandler}
+                    //original==onChange={(evt)=>setImage(evt.target.files[0])}
+                    />
+                </figure>} */}
+                {/* header with  Title Input, profile name, date*/}
+                {/* <header>
+                    <Input 
+                    type='Text' 
+                    placeholder='Post Title' 
+                    value={postTitle} 
+                    onChange={(evt)=>setPostTitle(evt.target.value)} 
+                    required/>
+                    <p> {user.username} </p>
+                    <p> {moment().format("MMM Do YYYY")}</p>
+                </header> */}
+
+                
+                {/* section with  species, success, date of the hunt*/}
+                {/* <section>
+
+                    <InputLabel>Date of Hunt</InputLabel>
+                        <Input 
+                            required 
+                            value={date} 
+                            onChange={(evt)=>setDate(evt.target.value)} 
+                            type='date'>                                
+                        </Input>
+
+                    <InputLabel>Species</InputLabel>
+                    <Input 
+                        type='text' 
+                        value={species} 
+                        onChange={(evt)=>setSpecies(evt.target.value)} 
+                        placeholder='Species' 
+                        required>                    
+                    </Input>
+                    
+                    <FormControl fullWidth>
+                        <InputLabel>Succesful Hunt?</InputLabel>
+                            <Select
+                                required
+                                labelId="successful-input-label"
+                                value={successful}
+                                label="successful-hunt"
+                                onChange={(evt)=>setSuccessful(evt.target.value)}
+                            >
+                                <MenuItem value={true}>Yes</MenuItem>
+                                <MenuItem value={false}>No</MenuItem>
+                            </Select>
+                    </FormControl>
+
+                </section> */}
+
+                {/* section with location, weapon, land type  */}
+                {/* <section>
+                    <FormControl fullWidth>
+                        <InputLabel required id="huntarea-input-label">Hunt Area</InputLabel>
+                            <Select
+                                labelId="huntarea-input-label"
+                                value={huntAreaId ? huntAreaId : ''}
+                                label="hunt-area"
+                                onChange={(evt)=>setHuntAreaId(evt.target.value)}
+                            >
+                                {huntAreaList.map((area)=>{
+                                    return <MenuItem key={area.id} value={area.id}>{area.hunt_area}</MenuItem>
+                                })}
+
+                            </Select>
+                    </FormControl>
+
+                    <InputLabel>Weapon Used</InputLabel>
+                    <Input type='text' value={weaponType} onChange={(evt)=>setWeaponType(evt.target.value)} placeholder='weapon-used'></Input>
+
+                    <FormControl fullWidth>
+                        <InputLabel id="land-type-input-label">Land Type</InputLabel>
+                            <Select
+                                labelId="land-type-input-label"
+                                value={landType}
+                                label="land-type"
+                                onChange={(evt)=>setLandType(evt.target.value)}
+                            >
+                                <MenuItem value={'public'}>Public</MenuItem>
+                                <MenuItem value={'private'}>Private</MenuItem>
+                            </Select>
+                    </FormControl>
+                </section> */}
+
+                {/* section with text input and store */}
+                {/* <section>
+                <TextareaAutosize
+                    required
+                    placeholder="Tell the story..."
+                    style={{ width: 500, height:200 }}
+                    value={story} onChange={(evt)=>setStory(evt.target.value)}
+                />
+
+                </section> */}
+                {/* eventually have a sweet alert for submit */}
+                {/* <button type="submit">Post</button>
+                <button type='submit' className='submitBtn'>Submit</button>
+                <button className='cancelBtn' onClick={()=>history.push(`/home`)}>Cancel</button>
+            </form> */}
+
+            {/* </article>
+            image cropper activated by change handler
+            {selectedFile ? 
+                <ImageCropper 
+                    id={selectedFile.id} 
+                    imageUrl={selectedFile.imageUrl}
+                    setFinalFile={setFinalFile}
+                    setSelectedFile={setSelectedFile}
+                    setImgUrl={setImgUrl}
+                    // setCroppedImageFor={setCroppedImageFor}
+                /> : null}
+        */}
