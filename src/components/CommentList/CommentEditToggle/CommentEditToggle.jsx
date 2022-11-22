40f4fcc39de5dff43c4ee4a6b27ccdc1ec9d865e
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-22 15:31:51 -0600

    comment buttons need to be centered

--- src/components/CommentList/CommentEditToggle/CommentEditToggle.jsx
+++ src/components/CommentList/CommentEditToggle/CommentEditToggle.jsx
@@ -2,7 +2,7 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-
+import { Grid } from '@mui/material';
 import moment from 'moment';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -27,9 +27,9 @@ function CommentEditToggle({user, post, deleteComment, comment, editComment}){
                         value={content} onChange={(evt)=>setContent(evt.target.value)}
                     />                  
                     {/* calls the edit comment function with props to edit it then resets the toggle to false */}
-                    <Button onClick={()=>{editComment(comment.id, content, comment.user_id), setEditOpen(false)}}>Submit</Button>
+                    <button onClick={()=>{editComment(comment.id, content, comment.user_id), setEditOpen(false)}}>Submit</button>
                     {/* on cancel reset toggle */}
-                    <Button onClick={()=>setEditOpen(false)}>Cancel</Button>
+                    <button onClick={()=>setEditOpen(false)}>Cancel</button>
                 </>
         )
     }
@@ -37,21 +37,26 @@ function CommentEditToggle({user, post, deleteComment, comment, editComment}){
         // if toggle isnt engaged show this
     return(
         <>
-                <><p><span className='commentUserName'>{comment.username}</span>  
-                <span className='commentDate'>{moment(comment.created).format("MMM Do YYYY")}</span></p>
-                <p className='commentContent'>{comment.content}</p></>
+                <Grid container spacing={2}>
+                    <Grid item sm={9}>
+                        <p><span className='commentUserName'>{comment.username}</span>  
+                        <span className='commentDate'>{moment(comment.created).format("MMM Do YYYY")}</span></p>
+                    </Grid>
+                    {(user.id===comment.user_id) ?
+                        
+                        <Grid item sm={3}>
+                            {/* OPENS TOGGLE above to allow an edit field and submittal */}
+                            <button className='editBtnComment' type="text" onClick={()=>setEditOpen(true)}>Edit</button>
+                            {/* calls the delete comment with id info */}
+                            <button className='deleteBtnComment' onClick={()=>deleteComment(comment.id)} type="text">Delete</button>
+                        </Grid>
+                        
+                        :
+                        <Grid item sm={3}></Grid>
+                    }
+                </Grid>
+                    <p className='commentContent'>{comment.content}</p>
             
-                {/* if the user owns the  comment the buttons are here and allow the user to delete or edit the post*/}
-                {(user.id===comment.user_id) ?
-                    <>
-                    {/* OPENS TOGGLE above to allow an edit field and submittal */}
-                    <button className='editBtnComment' type="text" onClick={()=>setEditOpen(true)}>Edit</button>
-                    {/* calls the delete comment with id info */}
-                    <button className='deleteBtnComment' onClick={()=>deleteComment(comment.id)} type="text">Delete</button>
-                    </>
-                    :
-                    null
-                }
         </>
 
         );
