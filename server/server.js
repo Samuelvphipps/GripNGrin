239fd55fd8ce32ecfabb6c202ee3503731f393f8
Sam Phipps <samuel.v.phipps@gmail.com>
2022-12-19 13:59:50 -0600

    image debug cont.

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use('/api/editPosts', editPostsRouter);
 app.use('/api/likes', likesRouter);
 
 // Serve static files
-app.use(express.static('build'));
+app.use(express.static('public'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
